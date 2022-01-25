a0c1a2b7925c49c87593e161bd39a6f9d2dfe339
Heru Handika <herubiolog@gmail.com>
2022-01-24 18:21:54 -0600

    Update rust code for mac.

--- scripts/rust_ffi.R
+++ scripts/rust_ffi.R
@@ -3,9 +3,17 @@ library(microbenchmark)
 
 p <- Sys.getenv("PATH")
 
+# For mac
+Sys.setenv(PATH = paste0("/Users/heruhandika/.cargo/bin:", p))
+
+# For Windows
 Sys.setenv(PATH = paste0("C:\\msys64\\mingw64\\bin", ";", p))
 
 rextendr::rust_function("
     fn multiply(x: f64, y: f64) -> f64 {
        x * y
     }")
+
+x = 1
+y = 2
+multiply(x,y)
